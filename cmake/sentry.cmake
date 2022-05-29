80f100c3e8c6aa01b7a36f01a647592899c15fa0
Dan Keenan <dk@dankeenan.org>
2022-05-29 17:23:25 -0400

    Fix Windows build with Sentry.

--- cmake/sentry.cmake
+++ cmake/sentry.cmake
@@ -5,6 +5,6 @@ FetchContent_Declare(
     GIT_TAG 0.4.17
 )
 
-set(SENTRY_BUILD_SHARED_LIBS Off)
+set(SENTRY_BUILD_SHARED_LIBS Off CACHE BOOL "" FORCE)
 FetchContent_MakeAvailable(sentry_native)
 target_compile_definitions(sentry INTERFACE SENTRY_BUILD_STATIC=1)
