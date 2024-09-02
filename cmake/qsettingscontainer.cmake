7266761b512f8594f370e3c1c18cd34cfd55f577
Dan Keenan <dk@dankeenan.org>
2024-09-02 11:38:44 -0400

    Fix CI issues.

--- cmake/qsettingscontainer.cmake
+++ cmake/qsettingscontainer.cmake
@@ -2,9 +2,6 @@ include(FetchContent)
 FetchContent_Declare(qsettingscontainer
     GIT_REPOSITORY "https://github.com/danielskeenan/QSettingsContainer.git"
     GIT_TAG "v1.0.0"
+    EXCLUDE_FROM_ALL
     )
-FetchContent_GetProperties(qsettingscontainer)
-if (NOT qsettingscontainer_POPULATED)
-    FetchContent_Populate(qsettingscontainer)
-    add_subdirectory(${qsettingscontainer_SOURCE_DIR} ${qsettingscontainer_BINARY_DIR} EXCLUDE_FROM_ALL)
-endif ()
+FetchContent_MakeAvailable(qsettingscontainer)
