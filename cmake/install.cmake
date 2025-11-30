9129090ff3d1f45d1654bd26833f3838e52e3f57
Dan Keenan <dk@dankeenan.org>
2025-11-30 12:04:22 -0500

    Fix installer in CI.

--- cmake/install.cmake
+++ cmake/install.cmake
@@ -13,6 +13,8 @@ set(CPACK_STRIP_FILES On)
 
 # Linux metadata
 if (CMAKE_SYSTEM_NAME STREQUAL "Linux")
+    set(CPACK_GENERATOR "DEB;RPM;TGZ")
+
     include(GNUInstallDirs)
     # .desktop file is filled in with configure-time info
     configure_file("${PROJECT_SOURCE_DIR}/meta/linux/${PROJECT_NAME}.desktop" "${PROJECT_BINARY_DIR}/meta/${PROJECT_NAME}.desktop")
@@ -28,10 +30,12 @@ if (CMAKE_SYSTEM_NAME STREQUAL "Linux")
     set(CPACK_DEBIAN_PACKAGE_SHLIBDEPS On)
     set(CPACK_DEBIAN_PACKAGE_SECTION "misc")
     set(CPACK_DEBIAN_PACKAGE_NAME "mobilesacn")
+    set(CPACK_RPM_PACKAGE_LICENSE "GPL-3.0-only")
 endif ()
 
 # Windows metadata
 if (CMAKE_SYSTEM_NAME STREQUAL "Windows")
+    set(CPACK_GENERATOR "WIX;ZIP")
     set(CPACK_PACKAGE_INSTALL_DIRECTORY "${CMAKE_PROJECT_NAME}")
     set(CPACK_RESOURCE_FILE_LICENSE "${PROJECT_BINARY_DIR}/LICENSE.rtf")
     set(CPACK_WIX_UPGRADE_GUID "A41ADB0F-BFDC-56C2-971C-517ACC3EB249")
