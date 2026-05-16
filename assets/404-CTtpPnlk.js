5a8267e0d5175f9e0fa7119bc064fe05a379f401
danielskeenan <danielskeenan@users.noreply.github.com>
2026-05-16 15:17:14 +0000

    deploy: fac5b8d51c29dce14abacc75851bd543fcca012b

---
+++ assets/404-CTtpPnlk.js
@@ -0,0 +1,1 @@
+import{t}from"./index-B0sn9tPw.js";var o=t(`<section class="text-gray-700 p-8"><h1 class="text-2xl font-bold">404: Not Found</h1><p class=mt-4>It's gone 😞`);function a(){return o()}export{a as default};
