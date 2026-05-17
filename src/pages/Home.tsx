d25c3b210fb889f2c2a61debe661647e7ae4ee6b
Dan Keenan <dk@dankeenan.org>
2026-05-16 20:24:02 -0400

    Light and Dark mode for screenshots.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,14 @@
 import {Title} from "@solidjs/meta";
-import {Button, Card, Col, Container, Figure, Row} from "solid-bootstrap";
+import {Button, Card, Col, Container, Row} from "solid-bootstrap";
 import Jumbotron from "@/components/Jumbotron";
 import logo from "@/assets/mobile_sacn.svg";
 import "./Home.scss";
-import chanCheckImg from "@/assets/chancheck_dark.png";
-import controlImg from "@/assets/control_keypad_dark.png";
-import viewLevelsImg from "@/assets/viewlevels_grid_dark.png";
+import chanCheckDarkImg from "@/assets/chancheck_dark.png";
+import chanCheckLightImg from "@/assets/chancheck_light.png";
+import controlDarkImg from "@/assets/control_keypad_dark.png";
+import controlLightImg from "@/assets/control_keypad_light.png";
+import viewLevelsDarkImg from "@/assets/viewlevels_grid_dark.png";
+import viewLevelsLightImg from "@/assets/viewlevels_grid_light.png";
 import {type Component} from "solid-js";
 import {A} from "@solidjs/router";
 import LINKS from "@/links.ts";
@@ -45,7 +48,12 @@ const Home: Component = () => {
                     <Col>
                         <Card>
                             <Card.Header>Channel Check</Card.Header>
-                            <Card.Img variant="top" src={chanCheckImg} alt="Screenshot of channel check mode"/>
+                            <Card.Img class="only-dark" variant="top" src={chanCheckDarkImg}
+                                      alt="Screenshot of channel check mode"
+                            />
+                            <Card.Img class="only-light" variant="top" src={chanCheckLightImg}
+                                      alt="Screenshot of channel check mode"
+                            />
                             <Card.Body>
                                 Run through addresses in any universe. Use per-address-priority to avoid affecting
                                 ranges you're not interested in.
@@ -55,8 +63,12 @@ const Home: Component = () => {
                     <Col>
                         <Card>
                             <Card.Header>Send Levels</Card.Header>
-                            <Card.Img variant="top" src={controlImg}
-                                      alt="Screenshot of live control mode's keypad interface"/>
+                            <Card.Img class="only-dark" variant="top" src={controlDarkImg}
+                                      alt="Screenshot of live control mode's keypad interface"
+                            />
+                            <Card.Img class="only-light" variant="top" src={controlLightImg}
+                                      alt="Screenshot of live control mode's keypad interface"
+                            />
                             <Card.Body>
                                 Adjust multiple channels in one universe using faders or a simple command-line
                                 interface.
@@ -66,7 +78,12 @@ const Home: Component = () => {
                     <Col>
                         <Card>
                             <Card.Header>View Levels</Card.Header>
-                            <Card.Img variant="top" src={viewLevelsImg} alt="Screenshot of view levels mode"/>
+                            <Card.Img class="only-dark" variant="top" src={viewLevelsDarkImg}
+                                      alt="Screenshot of view levels mode"
+                            />
+                            <Card.Img class="only-light" variant="top" src={viewLevelsLightImg}
+                                      alt="Screenshot of view levels mode"
+                            />
                             <Card.Body>
                                 View active levels from multiple sources.
                             </Card.Body>
